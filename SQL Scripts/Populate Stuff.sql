0edac497fe35d77895542e906513067a4605d530
Jed Dawson <jedd@ccscorp.com>
2012-05-09 21:24:06 -0700

    modified populate stuff to include employee id on create and use it as a varchar

--- SQL Scripts/Populate Stuff.sql
+++ SQL Scripts/Populate Stuff.sql
@@ -14,8 +14,8 @@ INSERT INTO timeclock.dbo.Department (Company_CompanyID, PayPeriodInterval, PayP
 VALUES (1, 14, 2012-05-09)
 GO
 
-INSERT INTO Employee (FirstName, LastName, DepartmentID, Terminated, EmployeeID)
-VALUES('Mikkel', 'Kringelbach', 1, 0, 1234)
+INSERT INTO Employee (EmployeeID, FirstName, LastName, DepartmentID, Terminated, EmployeeID)
+VALUES('10-MKRINGELBACH', 'Mikkel', 'Kringelbach', 1, 0, '10-MKRINGELBACH')
 GO
 
 INSERT INTO PunchType (Description)
@@ -31,15 +31,15 @@ VALUES('Vacation')
 GO
 
 INSERT INTO Punch (EmployeeID, InTime, OutTime, DepartmentID, PunchTypeID)
-VALUES (1234, '2012-05-09 8:00', '2012-05-09 12:30', 1, 1)
+VALUES ('10-MKRINGELBACH', '2012-05-09 8:00', '2012-05-09 12:30', 1, 1)
 GO
 
 INSERT INTO Punch (EmployeeID, InTime, OutTime, DepartmentID, PunchTypeID)
-VALUES (1234, '2012-05-09 13:00', '2012-05-09 17:30', 1, 1)
+VALUES ('10-MKRINGELBACH', '2012-05-09 13:00', '2012-05-09 17:30', 1, 1)
 GO
 
 INSERT INTO Timecard (EmployeeID, PayPeriod)
-VALUES (1234, 1)
+VALUES ('10-MKRINGELBACH', 1)
 GO
 
 INSERT INTO Line (PunchID, PayTypeID, SplitStart, SplitEnd, TimecardID)
