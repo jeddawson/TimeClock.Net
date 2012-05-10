9a94ace2208c34a7ba76354a350d3a15e8fe2713
Mikkel Kringelbach <Mikkel.Kringelbach@gmail.com>
2012-05-09 21:10:52 -0700

    queries to populate the DB with some data

---
+++ SQL Scripts/Populate Stuff.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO timeclock.dbo.PayType (DailyMax, WeeklyMax, Description)
+VALUES (24, 168, 'Overtime')
+GO
+INSERT INTO timeclock.dbo.Holiday (Date, Description, Repeats)
+VALUES (02-09-2012, 'TestHoliday', 0);
+GO
+
+INSERT INTO timeclock.dbo.Company (Name)
+VALUES	('TestCompany')
+GO
+
+INSERT INTO timeclock.dbo.Department (Company_CompanyID, PayPeriodInterval, PayPeriodSeed)
+VALUES (1, 14, 2012-05-09)
+GO
+
+INSERT INTO Employee (FirstName, LastName, DepartmentID, Terminated, EmployeeID)
+VALUES('Mikkel', 'Kringelbach', 1, 0, 1234)
+GO
+
+INSERT INTO PunchType (Description)
+VALUES('Normal')
+GO
+
+INSERT INTO PunchType (Description)
+VALUES('Sick')
+GO
+
+INSERT INTO PunchType (Description)
+VALUES('Vacation')
+GO
+
+INSERT INTO Punch (EmployeeID, InTime, OutTime, DepartmentID, PunchTypeID)
+VALUES (1234, '2012-05-09 8:00', '2012-05-09 12:30', 1, 1)
+GO
+
+INSERT INTO Punch (EmployeeID, InTime, OutTime, DepartmentID, PunchTypeID)
+VALUES (1234, '2012-05-09 13:00', '2012-05-09 17:30', 1, 1)
+GO
+
+INSERT INTO Timecard (EmployeeID, PayPeriod)
+VALUES (1234, 1)
+GO
+
+INSERT INTO Line (PunchID, PayTypeID, SplitStart, SplitEnd, TimecardID)
+VALUES (1, 2, '2012-05-09 8:00', '2012-05-09 12:30', 1)
+GO
+
+INSERT INTO Line (PunchID, PayTypeID, SplitStart, SplitEnd, TimecardID)
+VALUES (2, 2, '2012-05-09 13:00', '2012-05-09 17:30', 1)
+GO
+
