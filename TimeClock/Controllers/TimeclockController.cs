aff605be9c2a069875529323656120aac66e9e6f
Mikkel Kringelbach <Mikkel.Kringelbach@gmail.com>
2012-05-24 23:35:00 -0700

    So I wrote the two controllers, and as fare as I can see they should get the job done. I am assuming that when a punch is unfinished we set the OutTime equal to NULL.

--- TimeClock/Controllers/TimeclockController.cs
+++ TimeClock/Controllers/TimeclockController.cs
@@ -10,59 +10,72 @@ namespace TimeClock.Controllers
 {
     public class TimeclockController : Controller
     {
-        //
-        // GET: /Timeclock/
 
         public ActionResult Index()
         {
             return View();
         }
-         /*
-          /* Looks up the employees current status
-           *
+
+        /* Looks up the employees current status
+         */
         public ActionResult Status(string id)
         {
-             using (var db = new TimeClockContext())
-             {
-                  PayPeriod payPeriod = LookupPayPeriod();
+            using (var db = new TimeClockContext())
+            {
+                PayPeriod payPeriod = LookupPayPeriod();
 
-                  var empPunches = db.Punches.Where(p => p.EmployeeID == id && p.InTime > payPeriod.Start && p.InTime < payPeriod.End);
+                var empPunches = db.Punches.Where(p => p.EmployeeID == id && p.InTime > payPeriod.Start && p.InTime < payPeriod.End);
+                if (empPunches.Count(p => p.OutTime == null) != 0)
+                    ViewBag.PunchDirection = "In";
+                else
+                    ViewBag.PunchDirection = "Out";
 
-                  foreach( Punch punch in empPunches) {
-                       
-                  }
-
-                  if(should be in)
-                  ViewBag.PunchDirection = "In";
-                  else
-                  ViewBag.PunchDirection = "Out";
-
-                  return PartialView();
-             }
+                return PartialView();
+            }
         }
 
         public ActionResult Timecard(string id)
         {
-             using (var db = new TimeClockContext())
-             {
+            using (var db = new TimeClockContext())
+            {
+                int lineNumberCounter = 1;
+                List<TimeCardView> timecard = new List<TimeCardView>();
+                PayPeriod payPeriod = LookupPayPeriod();
 
-                  List<Line> timecard = new List<Line>();
-                  PayPeriod payPeriod = LookupPayPeriod();
+                var empTC = db.Timecards.SingleOrDefault(t => t.EmployeeID == id && t.PayPeriod == payPeriod.Start);
 
-                  var empTC = db.Timecards.SingleOrDefault( t => t.EmployeeID == id && t.PayPeriod == payPeriod.Start);
+                var lines = db.Lines.Where(l => l.TimecardID == empTC.TimecardID);
 
-                  var lines = db.Lines.Where(l => l.TimecardID == empTC.TimecardID);
+                lines.OrderBy(l => l.SplitStart);
 
-                  lines.OrderBy(l => l.SplitStart);
+                foreach (Line line in lines)
+                {
+                    // If the previous line contains information about the same punch, the we update it.
+                    if (timecard.Count > 0 && timecard[timecard.Count - 1].Out == line.SplitStart) 
+                    {
+                        timecard[timecard.Count - 1].Out = line.SplitEnd;
+                        timecard[timecard.Count - 1].Overtime = line.SplitStart.Subtract(line.SplitEnd).TotalHours;
+                        timecard[timecard.Count - 1].updateEntry();
+                    }
+                    // Otherwise we create a new line and add it to the timecard.
+                    else
+                    {
+                        TimeCardView temp = new TimeCardView(lineNumberCounter, line.SplitStart.Date, line.SplitStart, line.SplitEnd);
+                        if (line.PayType.Description == "Regular")
+                            temp.Regular = line.SplitStart.Subtract(line.SplitEnd).TotalHours;
+                        else if (line.PayType.Description == "Overtime")
+                            temp.Overtime = line.SplitStart.Subtract(line.SplitEnd).TotalHours;
+                        else
+                            ;// What should we do if it is neither of the two?
 
-                  foreach (Line line in lines)
-                  {
-                       line.PayType.
-                  }
+                        timecard.Add(temp);
+                    }
 
-                  return PartialView(timecard);
-             }
+                }
+
+                return PartialView(timecard);
+            }
         }
-         */
+
     }
 }
--- TimeClock/Resources/Objects.cs
+++ TimeClock/Resources/Objects.cs
@@ -19,9 +19,46 @@ namespace TimeClock.Resources
           public DateTime In { get; set; }
           public DateTime Out { get; set; }
           public TimeSpan Entry { get; set; }
+          public double Regular { get; set; }
+          public double Overtime { get; set; }
+
+         public TimeCardView(int lineNumber, DateTime date, DateTime intime, DateTime outtime, double regular, double overtime) 
+          {
+            lineNum = lineNumber;
+            Date = date;
+            In = intime;
+            Out = outtime;
+
+            Entry = Out.Subtract(In);
+
+            Regular = regular;
+            Overtime = overtime;
+
+          }
+
+         public TimeCardView(int lineNumber, DateTime date, DateTime intime, DateTime outtime)
+         {
+             lineNum = lineNumber;
+             Date = date;
+             In = intime;
+             Out = outtime;
+
+             Regular = 0;
+             Overtime = 0;
+
+             Entry = Out.Subtract(In);
+         }
+
+         public void updateEntry() 
+         {
+            Entry = Out.Subtract(In);
+         }
+
+
 
           TimeCardView()
           {
+                
                Entry = Out.Subtract(In);
           }
      }
