263512232384456b80ce8d00463859065c85071c
Jed Dawson <jedd@ccscorp.com>
2012-05-27 15:45:21 -0700

    Added in payperiod tools with pay period lookup function

---
+++ TimeClock/Resources/PayPeriod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TimeClock.Models;
+
+namespace TimeClock.Resources
+{
+    public class PayPeriodTools
+    {
+        /* just whipped this up... need to test the logic and make sure we get the first day of the current pay period! */
+        public static PayPeriod LookupPayPeriod(TimeClockContext db, int DepartmentID)
+        {
+            var seed = db.Departments.SingleOrDefault(d => d.DepartmentID == DepartmentID);
+
+            DateTime seedDate = seed.PayPeriodSeed;
+            int interval = seed.PayPeriodInterval;
+
+            TimeSpan span = DateTime.Now.Subtract(seedDate);
+
+            int count = (int) Math.Floor(span.TotalDays / (double) interval);
+
+            PayPeriod payPeriod = new PayPeriod();
+            payPeriod.Start = seedDate.AddDays(count * interval);
+
+            payPeriod.End = seedDate.AddDays( ( (count + 1) * interval ) - 1 ) ;
+
+            return payPeriod;
+        }
+
+    }
+}
