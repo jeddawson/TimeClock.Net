4119539ed92ac94a4ba98ba5ed7e27ec52506bde
Jed Dawson <jedd@ccscorp.com>
2012-05-27 23:18:42 -0700

    A couple changes to the site and added in a bunch of screenshots to the documentation

---
+++ TimeClock/Controllers/PunchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimeClock.Controllers
+{
+    public class PunchController : Controller
+    {
+        //
+        // GET: /Punch/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
